3634965f63825d04164fb27c8637718d10121d13
Brad Pfannmuller <bradpf@outlook.com>
2017-09-21 12:02:10 -0600

    Removing core features \nTo be added later using a proper review process

--- src/controllers/authentication.ts
+++ src/controllers/authentication.ts
@@ -3,9 +3,6 @@ import createRouter from '@/utilities/functions/createRouter';
 const router = createRouter();
 
 router.post('/local', (res, req) => {
-  if (!res.body.username || !res.body.password) {
-
-  }
 
 });
 
